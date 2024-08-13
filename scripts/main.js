61c1003edcc6be88d5d2d7a0d1bde0f68c0e36c0
catborn <itnoz@yandex.ru>
2024-08-13 20:12:59 +0300

    add gallery liza

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,65 @@
+document.addEventListener("DOMContentLoaded", () => {
+  setupHarvardSearch(); // Инициализация Harvard поиска при загрузке страницы
+  setupClevelandSearch(); // Инициализация Cleveland поиска при загрузке страницы
+  setupAustralia(); // Инициализация Australia
+  setupRijksmuseumSearch(); // Инициализация Rijks
 
+  const museumAll = document.getElementById("museum__all");
+  const museumOne = document.getElementById("museum__one");
+  const museumTwo = document.getElementById("museum__two");
+  const museumThree = document.getElementById("museum__three");
+  const museumFour = document.getElementById("museum__four");
+  // Показываем контейнер "All" при загрузке страницы
+  museumAll.classList.remove("hidden");
+  museumOne.classList.add("hidden");
+  museumTwo.classList.add("hidden");
+  museumThree.classList.add("hidden");
+  museumFour.classList.add("hidden");
 
+  // Обработчики переключения вкладок
+  const tabButtons = document.querySelectorAll('input[name="tab-btn"]');
+
+  tabButtons.forEach((button) => {
+    button.addEventListener("change", function () {
+      switch (this.id) {
+        case "__menu_tab-btn-1": // "All"
+          museumAll.classList.remove("hidden");
+          museumOne.classList.add("hidden");
+          museumTwo.classList.add("hidden");
+          museumThree.classList.add("hidden");
+          museumFour.classList.add("hidden");
+          break;
+        case "__menu_tab-btn-2": // "Harvard"
+          museumAll.classList.add("hidden");
+          museumOne.classList.remove("hidden");
+          museumTwo.classList.add("hidden");
+          museumThree.classList.add("hidden");
+          museumFour.classList.add("hidden");
+          break;
+        case "__menu_tab-btn-3": // "Cleveland"
+          museumAll.classList.add("hidden");
+          museumOne.classList.add("hidden");
+          museumTwo.classList.remove("hidden");
+          museumThree.classList.add("hidden");
+          museumFour.classList.add("hidden");
+          break;
+        case "__menu_tab-btn-4": //Australia
+          museumAll.classList.add("hidden");
+          museumOne.classList.add("hidden");
+          museumTwo.classList.add("hidden");
+          museumThree.classList.remove("hidden");
+          museumFour.classList.add("hidden");
+          break;
+        case "__menu_tab-btn-5": //Rijsk
+          museumAll.classList.add("hidden");
+          museumOne.classList.add("hidden");
+          museumTwo.classList.add("hidden");
+          museumThree.classList.add("hidden");
+          museumFour.classList.remove("hidden");
+          break;
+        default:
+          break;
+      }
+    });
+  });
+});
--- scripts/museum1.js
+++ scripts/museum1.js
@@ -1,43 +1,64 @@
-document.addEventListener("DOMContentLoaded", () => {
+const setupHarvardSearch = () => {
   const btn_museum1 = document.getElementById("__menu_tab-btn-2");
 
-  btn_museum1.addEventListener("click", async function () {
-    let gallery__section = document.getElementById("gallery");
+  // Создание и добавление нового контейнера, если его нет
+  let museum__one = document.getElementById("museum__one");
+  let existingContainer = museum__one.querySelector("#museum__container");
 
-    // Создание и добавление нового контейнера
+  if (!existingContainer) {
     let div_museum1 = document.createElement("div");
     div_museum1.classList.add("container");
     div_museum1.id = "museum__container";
     div_museum1.innerHTML = `
-      <form id="museum__search">
+      <form id="museum__search_one">
         <div class="museum__container">
-          <h3 class="museum__title">Harvard Art Museum</h3>
-          <div id="museum__search_input-button">
-          <input id="museum__search-input" name="search-input" type="text" value="">
-          <button type="submit" id="museum__search-button"></button>\n
-          <p class="museum__search-hint">
-            * Если хотите искать по classifications введите: Paintings/Sculpture/Photographs...
-          </p>
+          <h3 class="museum__title"><a href='https://harvardartmuseums.org/' target="_blank">Harvard Art Museum</a></h3>
+          <div class="museum__about">
+          <span class="museum__about_title">About the Museum</span>
+            <p>Visitors can enjoy the many amenities that greet 
+            them upon entering the museums’ beautifully 
+            restored Calderwood Courtyard on the first floor—our 
+            own version of a town square.</br>In addition to the galleries,
+             visitors can view works of art not currently on display 
+             by appointment in the expansive Art Study Center. 
+             Conveniently browse the collections and make an appointment.   
+            </p>
           </div>
-          
+          <div id="museum__search_one_input-button">
+            <input id="museum__search_one-input" name="search-input" type="text" value="" autocomplete='off'>
+            <button type="submit" id="museum__search_one-button"></button>
+            <p class="museum__search-hint">
+              * If you want to search by classifications, enter: Paintings/Sculpture/Photographs
+            </p>
+          </div>
+        </div>
+        <div class="museum__search_one-result">
+          <!-- Результаты поиска будут добавлены сюда -->
         </div>
       </form>
-      <div class="museum__search-result">
-        <!-- Результаты поиска будут добавлены сюда -->
-      </div>`;
-    gallery__section.appendChild(div_museum1);
+    `;
+    museum__one.appendChild(div_museum1);
+  }
 
-    // Добавление обработчика для отправки формы
-    document
-      .getElementById("museum__search")
-      .addEventListener("submit", async (event) => {
-        event.preventDefault();
-        await searchHarvardRecords();
-      });
+  // Обработчик клика на кнопку
+  btn_museum1.addEventListener("click", async function () {
+    // Инициализация поиска
+    if (document.getElementById("museum__search_one")) {
+      document
+        .getElementById("museum__search_one")
+        .addEventListener("submit", async (event) => {
+          event.preventDefault();
+          await searchHarvardRecords();
+          // Очистка значения инпута после отправки формы
+          document.getElementById("museum__search_one-input").value = "";
+        });
+    }
   });
 
   const searchHarvardRecords = async () => {
-    const searchQuery = document.getElementById("museum__search-input").value;
+    const searchQuery = document.getElementById(
+      "museum__search_one-input"
+    ).value;
     const apiKey = "19c3f5ed-fa29-417d-8f60-60d2c8d4d25e";
 
     // URL для первого запроса, чтобы узнать количество страниц
@@ -89,7 +110,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const displayResult = (finalJson) => {
     // Отображаем результаты поиска
-    const resultContainer = document.querySelector(".museum__search-result");
+    const resultContainer = document.querySelector(
+      ".museum__search_one-result"
+    );
     resultContainer.innerHTML = ""; // Очистка предыдущих результатов
 
     if (finalJson.records && finalJson.records.length > 0) {
@@ -101,7 +124,7 @@ document.addEventListener("DOMContentLoaded", () => {
           img.classList.add("museum__picture");
           img.src = record.primaryimageurl;
           const label_img = document.createElement("label");
-          label_img.textContent = record.title;
+          label_img.textContent = record.title || "No Title";
           divForImgLabel.appendChild(img);
           divForImgLabel.appendChild(label_img);
           resultContainer.appendChild(divForImgLabel);
@@ -111,4 +134,4 @@ document.addEventListener("DOMContentLoaded", () => {
       resultContainer.innerHTML = "<p>Нет результатов для вашего запроса.</p>";
     }
   };
-});
+};
