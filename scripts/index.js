deae9e93334de84120c247e630b049d89679e72c
Ekaterina1Kuzmina <k9647030712@ya.ru>
2024-08-16 11:27:42 +0200

    add media for footer

--- scripts/index.js
+++ scripts/index.js
@@ -129,7 +129,7 @@ async function getQoute() {
   qouteTwoParagraph.textContent = JSON.stringify(data[0].author).slice(1, -1);
   qouteOneParagraph.style.fontFamily = "Avenir-Bold";
   qouteTwoParagraph.style.fontFamily = "Avenir-Regular";
-  qouteOneParagraph.style.marginTop = "10%";
+  qouteOneParagraph.style.marginTop = "2%";
 }
 
 // finish script for block footer
